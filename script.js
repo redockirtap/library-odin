90b547247e6bb569f2c61b5b8007160ce9f7ae5a
Partrickoder <redockirtap@gmail.com>
2022-10-18 21:16:19 +0300

    Fix delete card button. Work on Read/Unread button.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const booksArray = [];
 const addBookButton = document.querySelector('.add-book');
 const modal = document.querySelector('.modal');
-const submitButton = document.querySelector('.submit-btn')
-let counter = 1;
+const submitButton = document.querySelector('.submit-btn');
+// const deleteButton = document.querySelector('.card-delete');
+let counter = 0;
 
 function Book(name, author, isRead) {
     this.name = name
@@ -53,19 +54,44 @@ const displayCard = function () {
     readBook.className = 'read-info'
 
     deleteBtn.textContent = '🗑';
-    cardInfo.innerText = `Book ${counter} 
-    "${booksArray.at(-1).name}",
-    ${booksArray.at(-1).author},
-    ${booksArray.at(-1).isRead}`;
+    cardInfo.innerText = `"${booksArray.at(-1).name}",
+    ${booksArray.at(-1).author}`;
     readBook.textContent = 'READ';
-    
+
     card.appendChild(deleteBtn);
     card.appendChild(cardInfo);
     card.appendChild(readBook);
     wrapper.appendChild(card);
+
+    deleteBtn.addEventListener('click', deleteCard);
+}
+
+const deleteCard = function (e) {
+    const theDeleteButton = e.target;
+    const bookIndex = Number(theDeleteButton.parentElement.className.slice(-1));
+    console.log(bookIndex);
+    booksArray.forEach((item, index) => changeIndex(item, index, bookIndex));
+    booksArray.splice(bookIndex, 1);
+    counter -= 1;
+    console.log(counter);
+    console.log(booksArray);
+    theDeleteButton.parentElement.remove();
+
+    
+}
+
+const changeIndex = function (item, index, bookIndex) {
+    // console.log(item, index, bookIndex);
+    const card = document.querySelector(`.cards.card-${index}`);
+    // console.log(card)
+    if (index > bookIndex) {
+        console.log(`I am card ${index}`);
+        card.className = `cards card-${index-1}`;
+    }
 }
 
 const eventListeners = function () {
+    // const deleteButton = document.querySelector('.card-delete');
     addBookButton.addEventListener('click', openModal);
     window.addEventListener('click', closeModal);
     submitButton.addEventListener('click', addTheBook);
